98e6a7193582d4ab160fd4b54a0245c9752b6527
kdbswo <kdbswo@gmail.com>
2024-05-30 22:35:45 +0900

    Add EditText

--- GitTasting/app/src/main/java/com/example/gittasting/MainActivity.kt
+++ GitTasting/app/src/main/java/com/example/gittasting/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.gittasting
 
 import android.os.Bundle
-import android.widget.*
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,6 +28,9 @@ class MainActivity : AppCompatActivity() {
         var textV : TextView = findViewById<TextView>(R.id.tv_cnt)
         textV.text = "전문전문 하고 움"
         println("testing")
+
+        val editText = findViewById<EditText>(R.id.et_et1)
+        textV.text = editText.text
 
     }
 }
